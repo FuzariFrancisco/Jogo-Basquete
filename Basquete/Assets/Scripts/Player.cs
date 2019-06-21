34cd213981ab05c73b7b2defec92c747294e2ca6
FuzariFrancisco <FranciscoFuzariDev@outlook.com>
2019-06-21 14:21:16 -0300

    jogo basquete 3d ainda não finalizado

---
+++ Basquete/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Bola bola;
+
+    public GameObject playerCamera;
+
+    public float distanciaBola = 2f;
+
+    public float ForcaArremesso = 550f;
+
+    public bool SegurandoBola = true;
+
+    // método Start é executado na criação do objeto
+    void Start()
+    {
+        bola.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SegurandoBola)
+        {
+            bola.transform.position = playerCamera.transform.position + playerCamera.transform.forward * distanciaBola;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                bola.AtivarTrilha();
+                SegurandoBola = false;
+                bola.GetComponent<Rigidbody>().useGravity = true;
+                bola.GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward * ForcaArremesso);
+            }
+        }
+    }
+}
